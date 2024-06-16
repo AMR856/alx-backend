441df1557fbab8c1d0401c68b1c094e2996de677
ameralnas <amer.live477@gmail.com>
2024-06-16 18:03:20 +0300

    Add those

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""BasicCache is created here as you can"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """I'm putting in here"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Getter here"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""FifoCaching is created here as you can"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.order_list = []
+
+    def put(self, key, item):
+        """Putter fucntion"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+            self.order_list.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                the_key_to_remove = self.order_list[0]
+                self.order_list = self.order_list[1:]
+                self.cache_data.pop(the_key_to_remove)
+                print(f'DISCARD: {the_key_to_remove}')
+
+    def get(self, key):
+        """Getter here"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
