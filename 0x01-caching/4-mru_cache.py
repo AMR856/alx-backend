0127c8171c1cd17657a0d291b5f4241c662b7fb5
ameralnas <amer.live477@gmail.com>
2024-06-16 19:46:11 +0300

    Waka Waka

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""FifoCaching is created here as you can"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """This class is doing a lot of things"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Getter here"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
