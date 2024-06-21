7386f8b4b7fb0377134abf520ab064d01c253419
ameralnas <amer.live477@gmail.com>
2024-06-21 20:13:36 +0300

    I see

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config:
     """Here you can find the configuration"""
     LANGUAGES = ["en", "fr"]
@@ -16,7 +24,6 @@ class Config:
 
 
 app.config.from_object(Config)
-
 
 def dict_maker():
     queries = request.query_string.decode('utf-8').split('&')
@@ -40,10 +47,25 @@ def get_locale():
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
+def get_user(id):
+    if id in users:
+        return users[id]
+    return None
+
+
+@app.before_request
+def before_request():
+    values_dict = dict_maker()
+    user_dict = {}
+    if 'login_as' in values_dict:
+        user_dict = get_user(int(values_dict['login_as']))
+        if user_dict is not None:
+            g.user = user_dict
+
 @app.route('/', strict_slashes=False)
 def main_page():
     """Just prints a simple messages"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
